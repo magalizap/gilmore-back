ab8546a69af8f8df4fc56d216ad433a94ba34891
Magali Sol <magalisol2307@gmail.com>
2023-09-29 19:46:31 -0300

    nuevo commit

--- src/controllers/carts.controllers.js
+++ src/controllers/carts.controllers.js
@@ -168,11 +168,7 @@ export const purchaseCart = async (req, res) => {
             success_url: `http://${req.headers.host}/api/payments/success?session_id={CHECKOUT_SESSION_ID}`,
             cancel_url: `http://${req.headers.host}/api/payments/cancel`,
         })
-        console.log(payment.success_url)
-        console.log(payment.cancel_url)
-        console.log(payment.url)
-        console.log(payment)
-
+        
         return res.redirect(payment.url)
         
     } catch (error) {
